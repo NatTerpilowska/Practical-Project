caaa88ad1b54e332b48129c9adf38fc8e0501fbc
Nat Terpilowska <natalieterpilowska@gmail.com>
2021-08-10 09:27:08 +0000

    Added Ansible Configs

--- service1/create.py
+++ service1/create.py
@@ -1,4 +1,11 @@
 from application import db
 
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:rooot@35.197.250.227/project2"
+
+db = SQLAlchemy(app)
 db.drop_all()
 db.create_all()
