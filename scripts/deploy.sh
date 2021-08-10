a7d593780570236895df50937a360c50184a865f
Nat Terpilowska <natalieterpilowska@gmail.com>
2021-08-10 11:02:13 +0000

    Added scripts

---
+++ scripts/deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+rsync -r docker-compose.yaml nginx swarm-manager:
+
+ssh swarm-manager <<EOF
+  export DATABASE=$DATABASE_URI
+  docker stack deploy --compose-file docker-compose.yaml character
+EOF
