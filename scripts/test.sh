35cb5f67a5c85a0046c4018ad44ed6b7cd7bc3c6
natterpilowska <natterpilowska@gmail.com>
2021-08-13 10:21:16 +0000

    Fixed tests

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Unit Testing
-python3 -m pytest --ignore-glob=service_1/tests/test_int.py --cov --cov-config=.coveragerc
+python3 -m pytest --ignore-glob=service_1/tests/test-se-1.py --cov --cov-config=.coveragerc
 
 # Integration Testing
 docker-compose up -d --build > /dev/null 2>&1 # silent
-docker exec service-1 python3 -m pytest tests/test_int.py --color=yes
+docker exec service_1 python3 -m pytest tests/test-se-1.py --color=yes
