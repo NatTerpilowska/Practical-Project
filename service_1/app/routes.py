c729de70d6ee26723aa0e652de258670c1682868
Natalie <natterpilowska@gmail.com>
2021-08-14 11:09:59 +0000

    nt

--- service_1/app/routes.py
+++ service_1/app/routes.py
@@ -9,19 +9,19 @@ def home():
     return render_template('home.html')
 
 
-#@app.route('/get')
-#def home():
-   # race = requests.get('http://service_2:5000/get/race').text
-  #  clas = requests.get('http://service_3:5000/get/class').text
-#
-  #  payload = {'race': race, 'clas': clas}
-  #  points = requests.post('http://service_4:5000/post/points', json=payload).json()
+@app.route('/get')
+def home():
+race = requests.get('http://service_2:5000/get/race').text
+clas = requests.get('http://service_3:5000/get/class').text
+
+payload = {'race': race, 'clas': clas}
+points = requests.post('http://service_4:5000/post/points', json=payload).json()
     
-  #  records = Character.query.order_by(Character.id.desc()).limit(5).all()
+records = Character.query.order_by(Character.id.desc()).limit(5).all()
 
     
-   # characters = Character(race=race, clas=clas, points=points)
-   # db.session.add(points)
-    #db.session.commit()
+characters = Character(race=race, clas=clas, points=points)
+db.session.add(points)
+db.session.commit()
 
-    #return render_template("home.html", points=points, records=records)
+return render_template("home.html", points=points, records=records)
