349df88a8af9216627fbe34fd69f7ada2ea42d61
Natalie <natterpilowska@gmail.com>
2021-08-14 12:07:24 +0000

    nt

--- service_1/app/routes.py
+++ service_1/app/routes.py
@@ -4,20 +4,18 @@ from app import app, db
 from app.models import Characters
 
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def home():
-    race = requests.get('http://service_2:5000/get/race').text
-    clas = requests.get('http://service_3:5000/get/class').text
+    race = requests.get('http://service_2:5000/get/race').json()
+    clas = requests.get('http://service_3:5000/get/class').json()
 
-    payload = {'race': race, 'clas': clas}
-    points = requests.post('http://service_4:5000/post/points',
-                           json=payload).json()
+    data = [race, clas]
+    points = requests.post('http://service_4:5000/post/points', json=data).json()
 
-    records = \
-        Character.query.order_by(Character.id.desc()).limit(5).all()
+    records = Character.query.order_by(Character.id.desc()).limit(15).all()
 
-    characters = Character(race=race, clas=clas, points=points)
-    db.session.add(points)
+    outcome = Character(race=race, clas=clas, points=points)
+    db.session.add(outcome)
     db.session.commit()
 
     return render_template('home.html', points=points, records=records)
