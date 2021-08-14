37ae315f1e4e44a80607e1b2f8f9c01ddc45a105
Natalie <natterpilowska@gmail.com>
2021-08-14 11:56:02 +0000

    nt

--- service_1/app/routes.py
+++ service_1/app/routes.py
@@ -2,21 +2,22 @@ from flask import Flask, request, jsonify, render_template
 import requests
 from app import app, db
 from app.models import Characters
- 
 
-@app.route("/")
+
+@app.route('/')
 def home():
-   race = [requests.get('http://service_2:5000/get/race').text]
-   clas = [requests.get('http://service_3:5000/get/class').text]
+    race = requests.get('http://service_2:5000/get/race').text
+    clas = requests.get('http://service_3:5000/get/class').text
 
-   payload = {'race': race, 'clas': clas}
-   points = requests.post('http://service_4:5000/post/points', json=payload).json()
-    
-   records = Character.query.order_by(Character.id.desc()).limit(5).all()
+    payload = {'race': race, 'clas': clas}
+    points = requests.post('http://service_4:5000/post/points',
+                           json=payload).json()
 
-    
-   characters = Character(race=race, clas=clas, points=points)
-   db.session.add(points)
-   db.session.commit()
+    records = \
+        Character.query.order_by(Character.id.desc()).limit(5).all()
 
-   return render_template("home.html", points=points, records=records)
+    characters = Character(race=race, clas=clas, points=points)
+    db.session.add(points)
+    db.session.commit()
+
+    return render_template('home.html', points=points, records=records)
