1e438b10e78383f8e2d394801d73e051176782aa
Natalie <natterpilowska@gmail.com>
2021-08-14 11:18:24 +0000

    fixed syntax

--- service_1/app/routes.py
+++ service_1/app/routes.py
@@ -6,22 +6,17 @@ from app.models import Characters
 
 @app.route("/")
 def home():
+   race = requests.get('http://service_2:5000/get/race').text
+   clas = requests.get('http://service_3:5000/get/class').text
 
-    return render_template('home.html')
-
-@app.route('/get')
-def home():
-race = requests.get('http://service_2:5000/get/race').text
-clas = requests.get('http://service_3:5000/get/class').text
-
-payload = {'race': race, 'clas': clas}
-points = requests.post('http://service_4:5000/post/points', json=payload).json()
+   payload = {'race': race, 'clas': clas}
+   points = requests.post('http://service_4:5000/post/points', json=payload).json()
     
-records = Character.query.order_by(Character.id.desc()).limit(5).all()
+   records = Character.query.order_by(Character.id.desc()).limit(5).all()
 
     
-characters = Character(race=race, clas=clas, points=points)
-db.session.add(points)
-db.session.commit()
+   characters = Character(race=race, clas=clas, points=points)
+   db.session.add(points)
+   db.session.commit()
 
-return render_template("home.html", points=points, records=records)
+   return render_template("home.html", points=points, records=records)
