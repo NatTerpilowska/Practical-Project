eece39d895b6a99fa4f6f60bf853bd751da737f1
Natalie <natterpilowska@gmail.com>
2021-08-14 14:27:52 +0000

    nt

--- service_2/app.py
+++ service_2/app.py
@@ -9,5 +9,6 @@ race = ['Dragonborn','Dwarf','Elf','Gnome','Half-Elf','Halfling','Half-Orc','Hum
 def get_race():
     return (random.choice(race)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
+ 
--- service_4/app.py
+++ service_4/app.py
@@ -71,4 +71,4 @@ def post_points():
     return jsonify(points)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
