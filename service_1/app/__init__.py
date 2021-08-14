77980e301c489d1e8245b21c617da48ce418058b
Natalie Terpilowska <86067593+NatTerpilowska@users.noreply.github.com>
2021-08-14 18:03:31 +0100

    Update __init__.py

--- service_1/app/__init__.py
+++ service_1/app/__init__.py
@@ -6,7 +6,9 @@ from os import getenv
 app = Flask(__name__)
 
 app.config.update(
-    SQLALCHEMY_DATABASE_URI=os.getenv("DATABASE_URI")
+    SQLALCHEMY_DATABASE_URI =getenv("DATABASE_URI"),
+    SQLALCHEMY_TRACK_MODIFICATIONS=False,
+    SECRET_KEY=str(os.urandom(16))
 )
 
 
