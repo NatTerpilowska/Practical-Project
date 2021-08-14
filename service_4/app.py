40031cade187f21e9a86bfafba4f6c63a28c76d2
Natalie <natterpilowska@gmail.com>
2021-08-14 20:35:26 +0000

    nt

--- service_4/app.py
+++ service_4/app.py
@@ -2,8 +2,7 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-
-strenght = {
+points = {
     'race': {
         'Dragonborn':2,'Dwarf':0,'Elf':0,'Gnome':0,'Half-Elf':0,'Halfling':0,'Half-Orc':2,'Human':1,'Tiefling':0
     },
@@ -13,61 +12,12 @@ strenght = {
     }
 }
 
-dexterity = {
-    'race': {
-        'Dragonborn':0,'Dwarf':0,'Elf':2,'Gnome':0,'Half-Elf':0,'Halfling':2,'Half-Orc':0,'Human':1,'Tiefling':0
-    },
-    'clas': {
-        'Barbarian':0,'Bard':0,'Cleric':0,'Druid':0,'Fighter':0,'Monk':0,'Paladin':0,'Ranger':0,'Rogue':0,'Sorcerer':0,'Warlock':0,'Wizard':0
-        
-    }
-}
-
-constitution = {
-    'race': {
-        'Dragonborn':0,'Dwarf':2,'Elf':0,'Gnome':0,'Half-Elf':0,'Halfling':0,'Half-Orc':1,'Human':1,'Tiefling':0
-    },
-    'clas': {
-        'Barbarian':0,'Bard':0,'Cleric':0,'Druid':0,'Fighter':0,'Monk':0,'Paladin':0,'Ranger':0,'Rogue':0,'Sorcerer':0,'Warlock':0,'Wizard':0
-    }
-}
-
-intelligence = {
-    'race': {
-        'Dragonborn':0,'Dwarf':0,'Elf':0,'Gnome':2,'Half-Elf':0,'Halfling':0,'Half-Orc':0,'Human':1,'Tiefling':1
-    },
-    'clas': {
-        'Barbarian':0,'Bard':0,'Cleric':0,'Druid':0,'Fighter':0,'Monk':0,'Paladin':0,'Ranger':0,'Rogue':0,'Sorcerer':0,'Warlock':0,'Wizard':0
-    }
-}
-
-wisdom = {
-    'race': {
-        'Dragonborn':0,'Dwarf':0,'Elf':0,'Gnome':0,'Half-Elf':0,'Halfling':0,'Half-Orc':0,'Human':1,'Tiefling':0
-    },
-    'clas': {
-        'Barbarian':0,'Bard':0,'Cleric':0,'Druid':0,'Fighter':0,'Monk':0,'Paladin':0,'Ranger':0,'Rogue':0,'Sorcerer':0,'Warlock':0,'Wizard':0  
-    }
-}
-charisma = {
-    'race': {
-        'Dragonborn':1,'Dwarf':0,'Elf':0,'Gnome':0,'Half-Elf':2,'Halfling':0,'Half-Orc':0,'Human':1,'Tiefling':2
-    },
-    'clas': {
-        'Barbarian':0,'Bard':0,'Cleric':0,'Druid':0,'Fighter':0,'Monk':0,'Paladin':0,'Ranger':0,'Rogue':0,'Sorcerer':0,'Warlock':0,'Wizard':0   
-    }
-}
 @app.route('/post/points', methods=['POST'])
 def post_points():
     stre = request.json['strenght']
-    dext = request.json['dexterity']
-    cons = request.json['constitution']
-    inte = request.json['intelligence']
-    wisd = request.json['wisdom']
-    char = request.json['charisma']
+   
 
-    points = strenght['strenght'][stre], dexterity['dexterity'][dext], constitution['constitution'][cons], intelligence['intelligence'][inte], wisdom['wisdom'][wisd], charisma['charisma'][char]
-
+    points = strenght['strenght'][stre]
     return jsonify(points)
 
 if __name__ == '__main__':
