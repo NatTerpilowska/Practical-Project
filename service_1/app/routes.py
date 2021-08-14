9c7a9d1a22f2c776fe4e9ce05fbf90917afd2de6
Natalie <natterpilowska@gmail.com>
2021-08-14 10:24:45 +0000

    nt trying to debug

--- service_1/app/routes.py
+++ service_1/app/routes.py
@@ -5,19 +5,25 @@ from app.models import Characters
  
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    race = requests.get('http://service_2:5000/get/race').text
-    clas = requests.get('http://service_3:5000/get/class').text
 
-    payload = {'race': race, 'clas': clas}
-    points = requests.post('http://service_4:5000/post/points', json=payload).json()
+    return render_template("home.html")
+
+
+#@app.route('/get')
+#def home():
+   # race = requests.get('http://service_2:5000/get/race').text
+  #  clas = requests.get('http://service_3:5000/get/class').text
+#
+  #  payload = {'race': race, 'clas': clas}
+  #  points = requests.post('http://service_4:5000/post/points', json=payload).json()
     
-    records = Character.query.order_by(Character.id.desc()).limit(5).all()
+  #  records = Character.query.order_by(Character.id.desc()).limit(5).all()
 
     
-    characters = Character(race=race, clas=clas, points=points)
-    db.session.add(points)
-    db.session.commit()
+   # characters = Character(race=race, clas=clas, points=points)
+   # db.session.add(points)
+    #db.session.commit()
 
-    return render_template("home.html", points=points, records=records)
+    #return render_template("home.html", points=points, records=records)
