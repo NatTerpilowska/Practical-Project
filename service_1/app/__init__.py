dfc4841f249b872e0aa2405e501fd53c449fceb1
Natalie <natterpilowska@gmail.com>
2021-08-14 19:13:29 +0000

    nt

--- service_1/app/__init__.py
+++ service_1/app/__init__.py
@@ -1,15 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-from os import getenv
 
 app = Flask(__name__)
 
-app.config.update(
-    SQLALCHEMY_DATABASE_URI=getenv("DATABASE_URI")
-)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
 
 
 db = SQLAlchemy(app)
 
-from . import routes
+from app import routes
