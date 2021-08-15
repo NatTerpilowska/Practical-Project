183b544026049472fc3e1ef4118f881ac0d0f9dc
Natalie <natterpilowska@gmail.com>
2021-08-15 14:06:20 +0000

    nt

--- service_1/app/__init__.py
+++ service_1/app/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+import os
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "DATABASE_URI"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URI")
 
 
 db = SQLAlchemy(app)
